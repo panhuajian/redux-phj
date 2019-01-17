a2f6c8af679da11c82eedb4a3614c40678be95db
panhuajian <panhj@cheche365.com>
2019-01-17 17:11:05 +0800

    添加注释，修改加减逻辑

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import store from './store'
 
+// 第一次默认赋值
 (function () {
   console.log('store', store)
   document.querySelector('.cont').innerHTML = store.getState().num
 })()
+// 往subscribe中添加监听函数
 store.subscribe(() => {
   document.querySelector('.cont').innerHTML = store.getState().num
 })
+// 加值
 document.querySelector('.addition').onclick = function () {
   let num = store.getState().num
-  num++
   store.dispatch({
     type: 'ADDITION',
-    data: num
+    data: ++num
   })
 }
+// 减值
 document.querySelector('.subtraction').onclick = function () {
   let num = store.getState().num
   num--
   store.dispatch({
     type: 'SUBTRACTION',
-    data: num
+    data: --num
   })
 }
