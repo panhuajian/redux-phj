25fe53494dd3c357e51c0c55905435858fd25ea5
panhuajian <panhj@cheche365.com>
2019-01-17 17:04:55 +0800

    添加一些注释

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -7,8 +7,10 @@ export default function (reducers, initState) {
   let currentListeners = []
   let isDispetch = false
 
+  // 默认第一次触发dispatch
   dispatch(defaultAction)
 
+  // 返回store的方法
   return {
     getState,
     dispatch,
@@ -22,14 +24,17 @@ export default function (reducers, initState) {
 
   // 触发action
   function dispatch (action) {
+    // 执行reducer，更新当前的state
     currentState = reducers(currentState, action)
-    currentListeners.forEach(item => {
-      item()
+    // 更新state之后，执行每个监听函数
+    currentListeners.forEach( listener => {
+      listener()
     })
   }
 
   // 点阅监听事件
   function subscribe (listener) {
+    // 将监听函数添加到数组中
     currentListeners.push(listener)
   }
 }
